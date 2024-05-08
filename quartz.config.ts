fb4065e1fe8102d6d4887581c95f29e51168aa5b
isendel <jacpresta@vivaldi.net>
2024-05-08 10:58:00 +0200

    Quartz sync: May 8, 2024, 10:58 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Swimmer's Shoulder Congress",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
